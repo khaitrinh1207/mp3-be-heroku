688053372ec9b61f382b8fd2719ab50791235584
khaitrinh1207 <khaitrinh1207@gmail.com>
2021-05-18 10:46:46 +0700

    upload

---
+++ src/main/java/com/example/demo/model/Category.java
@@ -0,0 +1,48 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String img;
+    private String nameCate;
+
+    public Category() {
+    }
+
+    public Category(Long id, String img, String nameCate) {
+        this.id = id;
+        this.img = img;
+        this.nameCate = nameCate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public String getNameCate() {
+        return nameCate;
+    }
+
+    public void setNameCate(String nameCate) {
+        this.nameCate = nameCate;
+    }
+}
